ddd782c004243ff69d6a1dc1f2045c5ee39754a9
Magda1109 <zyla.magdalena.92@gmail.com>
2023-07-04 20:13:31 +0200

    Add project files.

---
+++ RestaurantApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestaurantApp;
+
+var stack = new Stack<double>();
+stack.Push(4.5);
+stack.Push(4);
+stack.Push(433.5);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
