99c52e1eadee9c69899740c3230f77803847e28a
Magda1109 <zyla.magdalena.92@gmail.com>
2024-02-12 21:14:34 +0100

    Stack implementation

--- RestaurantApp/Program.cs
+++ RestaurantApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestaurantApp;
+
+var stack = new BasicStack<double>();
+stack.Push(4.5);
+stack.Push(43);
+stack.Push(222.0);
+
+double sum = 0.0;
+
+while(stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Items: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
